d877cb21724433faaf270c49f1c42479739e4571
iaammsk303 <133005788+iaammsk303@users.noreply.github.com>
2025-03-12 23:51:23 +0530

    cors error

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
 const cors = require('cors')
 const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
 require('dotenv').config()
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000;
-
-// middleware setup
-app.use(express.json({limit: "25mb"}));
-// app.use((express.urlencoded({limit: "25mb"})));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors({
-    origin: true, // Allow all origins
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}))
 
 // Handle preflight requests for all routes
 app.options('*', cors());
