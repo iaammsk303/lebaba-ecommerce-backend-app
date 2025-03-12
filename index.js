c204d158e54057fcaa6a094d6661d0ae8b974dd0
iaammsk303 <133005788+iaammsk303@users.noreply.github.com>
2025-03-12 23:18:17 +0530

    cors changes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
         'https://lebaba-frontend-final-fdvqfyq5s-mohammed-saadiq-k-hans-projects.vercel.app'
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
