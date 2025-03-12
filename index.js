79dcad0e3aeb15deacb24079a446a53ff2fbacf6
iaammsk303 <133005788+iaammsk303@users.noreply.github.com>
2025-03-13 00:07:00 +0530

    the changes before

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const cors = require('cors')
-const app = express();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors')
+const app = express();
 require('dotenv').config()
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000;
 
-// Handle preflight requests for all routes
-app.options('*', cors());
+// middleware setup
+app.use(express.json({limit: "25mb"}));
+// app.use((express.urlencoded({limit: "25mb"})));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+    origin: 'https://lebaba-frontend-final-lime.vercel.app',
+    credentials: true
+}))
 
 // image upload 
 const uploadImage = require("./src/utils/uploadImage")
@@ -26,18 +34,20 @@ app.use("/api/reviews", reviewRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/stats', statsRoutes)
 
+
+
 main()
   .then(() => console.log("mongodb is successfully connected."))
   .catch((err) => console.log(err));
 
-async function main() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB is successfully connected.");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
+  async function main() {
+    try {
+      await mongoose.connect(process.env.DB_URL);
+      console.log("MongoDB is successfully connected.");
+    } catch (error) {
+      console.error("MongoDB connection error:", error);
+    }
   }
-}
 
 app.post("/uploadImage", (req, res) => {
   uploadImage(req.body.image)
